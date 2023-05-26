60f9fba47f6c69d49398fcbe55c2468f53cde9a9
Jeswant <67511959+Jeswantts@users.noreply.github.com>
2023-05-26 18:46:31 +0530

    Add files via upload
    Added model-class folder

---
+++ ModelLibrary/Models/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Models
+{
+    public class Hotel
+    {
+        [Key]
+        public int HotelId { get; set; }
+        public string? HotelName { get; set; }
+        public string? HotelLocation { get; set;}
+        public string? HotelType { get; set; }
+        public string? Feedback { get; set; }
+        public ICollection<Room>? Rooms { get; set; }
+
+    }
+}
